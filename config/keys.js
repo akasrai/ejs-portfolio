fbf726f188c3366d6a26832dee398f0332db8219
akash <akasky70@gmail.com>
2019-07-05 23:49:15 +0545

    setup login signup feature

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb://localhost:27017/' + encodeURIComponent('') + 'mongo_db';
+
+module.exports = {
+  mongoURI: dbPassword
+};
---
+++ models/blog.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const BlogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  slug: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  },
+  updatedOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Blog = mongoose.model('Blog', BlogSchema);
+
+module.exports = Blog;
